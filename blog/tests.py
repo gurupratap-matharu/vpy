6c6be01c08a62cb39c03e9c8176b98a73f413a2f
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2025-06-11 16:28:36 -0300

    Implement basic unit tests for blog models

--- blog/tests.py
+++ blog/tests.py
@@ -1,7 +1,11 @@
 import logging
+from unittest import skip
+
+from django.utils import timezone
 
 from wagtail.models import Page, Site
 from wagtail.test.utils import WagtailPageTestCase
+from wagtail.test.utils.form_data import nested_form_data, streamfield
 
 from blog.models import BlogIndexPage, BlogPage
 from home.models import HomePage
@@ -13,6 +17,8 @@ class BlogIndexPageTests(WagtailPageTestCase):
     """
     Test suite to check if the blog index page is routable under different routes.
     """
+
+    template_name = "blog/blog_index_page.html"
 
     @classmethod
     def setUpTestData(cls):
@@ -26,11 +32,8 @@ class BlogIndexPageTests(WagtailPageTestCase):
             pass
 
         cls.root = Page.objects.get(id=1).specific
-        cls.home_page = HomePage(
-            title="Home", slug="home", hero_text="You can do it", hero_cta="Learn More"
-        )
+        cls.home_page = HomePage(title="Home", slug="home")
         cls.blog_index_page = BlogIndexPage(title="blog", slug="blog")
-        cls.blog_page = BlogPage(title="Buenos Aires", slug="buenos-aires")
 
         # Set Home Page as child of root
         cls.root.add_child(instance=cls.home_page)
@@ -46,19 +49,17 @@ class BlogIndexPageTests(WagtailPageTestCase):
 
         # Add Blog Index as child of Home Page
         cls.home_page.add_child(instance=cls.blog_index_page)
+        cls.blog_index_page.first_published_at = timezone.now()
+        cls.blog_index_page.last_published_at = timezone.now()
         cls.blog_index_page.save_revision().publish()
         cls.blog_index_page.save()
 
-        # Add BlogPage as child of BlogIndex
-        # Veer you need a blog page as the blog index has a featured blog post section
-        # so atleast one blog page is needed. Temp fix for now
-        cls.blog_index_page.add_child(instance=cls.blog_page)
-        cls.blog_page.save_revision().publish()
-        cls.blog_page.save()
-
     def test_get(self):
         response = self.client.get(self.blog_index_page.url)
+
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, self.template_name)
+        self.assertNotContains(response, "Hi I should not be on this page")
 
     def test_default_route(self):
         self.assertPageIsRoutable(self.blog_index_page)
@@ -72,9 +73,8 @@ class BlogIndexPageTests(WagtailPageTestCase):
     def test_editability(self):
         self.assertPageIsEditable(self.blog_index_page)
 
-    # Not sure why this test doesn't pass!
-    # def test_general_previewability(self):
-    #     self.assertPageIsPreviewable(self.blog_index_page)
+    def test_general_previewability(self):
+        self.assertPageIsPreviewable(self.blog_index_page)
 
     def test_can_create_blog_index_under_home_page(self):
         self.assertCanCreateAt(HomePage, BlogIndexPage)
@@ -98,6 +98,8 @@ class BlogPageTests(WagtailPageTestCase):
     Test suite for the blog post page.
     """
 
+    template_name = "blog/blog_page.html"
+
     @classmethod
     def setUpTestData(cls):
         try:
@@ -110,9 +112,7 @@ class BlogPageTests(WagtailPageTestCase):
             pass
 
         cls.root = Page.objects.get(id=1).specific
-        cls.home_page = HomePage(
-            title="Home", slug="home", hero_text="You can do it", hero_cta="Learn More"
-        )
+        cls.home_page = HomePage(title="Home", slug="home")
         cls.blog_index_page = BlogIndexPage(title="blog", slug="blog")
         cls.blog_page = BlogPage(title="Buenos Aires", slug="buenos-aires")
 
@@ -128,17 +128,56 @@ class BlogPageTests(WagtailPageTestCase):
 
         # Add BlogIndex as child of Home Page
         cls.home_page.add_child(instance=cls.blog_index_page)
+
+        cls.blog_index_page.first_published_at = timezone.now()
+        cls.blog_index_page.last_published_at = timezone.now()
         cls.blog_index_page.save_revision().publish()
         cls.blog_index_page.save()
 
         # Add BlogPage as child of BlogIndex
         cls.blog_index_page.add_child(instance=cls.blog_page)
+
+        cls.blog_page.first_published_at = timezone.now()
+        cls.blog_page.last_published_at = timezone.now()
         cls.blog_page.save_revision().publish()
         cls.blog_page.save()
+
+    def _get_post_data(self):
+        data = dict()
+        data["title"] = "Things to do in Buenos Aires"
+        data["body"] = streamfield([("text", "come see the obelisco")])
+
+        return nested_form_data(data)
 
     def test_get(self):
         response = self.client.get(self.blog_page.url)
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, self.template_name)
+        self.assertNotContains(response, "Hi I should not be on this page")
 
     def test_default_route(self):
         self.assertPageIsRoutable(self.blog_page)
+
+    def test_page_is_renderable(self):
+        self.assertPageIsRenderable(self.blog_page)
+
+    @skip("veer cannot figure out how to pass this 🤔")
+    def test_page_is_previewable(self):
+        # Veer this test is failing probably because we are not providing author for
+        # blog page. At this moment i don't know how to do it.
+        post_data = self._get_post_data()
+        self.assertPageIsPreviewable(self.blog_page, post_data=post_data)
+
+    def test_editability(self):
+        post_data = self._get_post_data()
+        self.assertPageIsEditable(self.blog_page, post_data=post_data)
+
+    def test_can_create_blog_page_under_blogindex_page(self):
+        self.assertCanCreateAt(parent_model=BlogIndexPage, child_model=BlogPage)
+
+    def test_cannot_create_wrong_children_or_parents_for_blog_page(self):
+        self.assertCanNotCreateAt(parent_model=BlogPage, child_model=BlogIndexPage)
+        self.assertCanNotCreateAt(parent_model=BlogPage, child_model=HomePage)
+
+    def test_blog_page_subpages(self):
+        self.assertAllowedSubpageTypes(parent_model=BlogPage, child_models={})
